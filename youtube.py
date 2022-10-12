b4fbfafbdd3f0a39d7ab1d96f5129a39bb644cc2
Jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2022-10-12 17:07:18 +0200

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -13,16 +13,16 @@ Id_Canal_Youtube = "UC1s7u4KXpKD1MBCvOVuZCQg" #IR al canal de vuestro Youtube fa
 url_youtube = "https://www.youtube.com/watch?v="
 
 @bot.command()
-async def hola(ctx): #Comando a decir
-    
+async def Youtube(ctx):
+    if ctx.message.channel.id in [1029771684280025098,1028165477782007838,1025238973288816690]: #Añadimos mas canales de vuestro servidor de discord
+        
+        response = requests.get(f"https://www.googleapis.com/youtube/v3/search?key={Api_key_Youtube}&channelId={Id_Canal_Youtube}&part=snippet,id&order=date&maxResults=20")
+        video = response.json()["items"][0]["snippet"]["title"]
+        id_video_youtube = response.json()["items"][0]["id"]["videoId"]
+        await ctx.send(f'{video}\n\n{url_youtube}{id_video_youtube}') 
 
-    response = requests.get(f"https://www.googleapis.com/youtube/v3/search?key={Api_key_Youtube}&channelId={Id_Canal_Youtube}&part=snippet,id&order=date&maxResults=20")
-
-    video = response.json()["items"][0]["snippet"]["title"]
-    id_video_youtube = response.json()["items"][0]["id"]["videoId"]
-
-
-    await ctx.send(f'{video}\n\n{url_youtube}{id_video_youtube}') #Mensaje que dira el bot
+    else:
+        await ctx.send("No puedes usar el comando en este canal")
  
  
 
